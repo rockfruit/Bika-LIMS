2decc59e1730a8f87b0386658d2e8635df2d0fb9
Mike Metcalfe <mike@webtide.co.za>
2017-02-09 13:45:14 +0200

    Use DateSampled to set SamplingDate on arimport

--- bika/lims/utils/sample.py
+++ bika/lims/utils/sample.py
@@ -28,6 +28,9 @@ def create_sample(context, request, values):
         sample.processForm(REQUEST=request, values=values)
         # Required to ensure that IAcquireFieldDefaults is honored
         sample.setDefaults()
+        # Specifically set the DateSampled
+        if 'DateSampled' in values:
+            sample.setSamplingDate(values['DateSampled'])
         # Perform the appropriate workflow action
         workflow_action =  'sampling_workflow' if workflow_enabled \
             else 'no_sampling_workflow'
