e5467a0800be698151dfdb1765c84d5077ea0b95
Campbell <campbell@bikalabs.com>
2017-10-04 13:09:26 +0200

    Cleanup to340 migration
    (cherry picked from commit aaeafb281ae822ca4771c7c4e1c1d3919f0fc1a1)
    
    # Conflicts:
    #	bika/lims/upgrade/to340.py

--- bika/lims/upgrade/to340.py
+++ bika/lims/upgrade/to340.py
@@ -38,6 +38,9 @@ def upgrade(tool):
     # Sync the empty number generator with existing content
     prepare_number_generator(portal)
 
+    # Migrate some references (Analysis->*) to new UIDReference
+    refactor_references()
+
     return True
 
 
@@ -135,3 +138,87 @@ def prepare_number_generator(portal):
     pc = portal.portal_catalog
     for brain in pc():
         generateUniqueId(brain.getObject())
+
+
+def refactor_references():
+    """
+    Many ReferenceFields and HistoryAwareReferenceFields need to be migrated
+    away from Archetypes ReferenceField.
+
+    UIDReferenceField uses simple StringFields to store the UIDs. After
+    refactoring, the following references were moved to UIDReference:
+
+    Analysis.Attachments
+    Analysis.Instrument
+    Analysis.Method
+    Analysis.SamplePartition
+    """
+    migrate_refs('AnalysisInstrument', 'Instrument')
+    migrate_refs('AnalysisMethod', 'Method')
+    migrate_refs('AnalysisAttachment', 'Attachment')
+    migrate_refs('AnalysisSamplePartition', 'SamplePartition')
+
+    refs_removed = 0
+    for rel in ['AnalysisInstrument',
+                'AnalysisMethod',
+                'AnalysisAttachment',
+                'AnalysisSamplePartition',
+                ]:
+        refs_removed += del_at_refs(rel)
+    if refs_removed:
+        logger.info("Total reference objects removed: %s" % refs_removed)
+
+
+def touidref(src, dst, src_relation, fieldname):
+    """Convert an archetypes reference in src/src_relation to a UIDReference
+    in dst/fieldname.
+    """
+    field = dst.getField(fieldname)
+    refs = src.getRefs(relationship=src_relation)
+    if len(refs) == 1:
+        value = get_uid(refs[0])
+    elif len(refs) > 1:
+        value = filter(lambda x: x, [get_uid(ref) for ref in refs])
+    else:
+        value = field.get(src)
+    if not value:
+        value = ''
+    if not field:
+        raise Exception('Cannot find field %s/%s' % (fieldname, src))
+    if field.required and not value:
+        logger.error('Required %s field %s/%s has no value' % (
+            src.portal_type, src, fieldname))
+    field.set(src, value)
+
+
+def migrate_refs(rel, fieldname, pgthreshold=100):
+    rc = get_tool('reference_catalog')
+    uc = get_tool('uid_catalog')
+    refs = rc(relationship=rel)
+    if refs:
+        logger.info('Migrating %s references of %s' % (len(refs), rel))
+    for i, ref in enumerate(refs):
+        obj = uc(UID=ref[1])
+        if obj:
+            obj = obj[0].getObject()
+            if i and not divmod(i, pgthreshold)[1]:
+                logger.info("%s/%s %s/%s" % (i, len(refs), obj, rel))
+            touidref(obj, obj, rel, fieldname)
+
+
+def del_at_refs(rel):
+    # Remove this relation from at_references
+    rc = get_tool('reference_catalog')
+    refs = rc(relationship=rel)
+    removed = 0
+    size = 0
+    if refs:
+        logger.info("Found %s refs for %s" % (len(refs), rel))
+        ref_dict = {ref[0]: ref.getObject() for ref in refs}
+        for ref_id, ref_obj in ref_dict.items():
+            removed += 1
+            size += 1
+            ref_obj.aq_parent.manage_delObjects([ref_id])
+    if removed:
+        logger.info("Performed %s deletions" % removed)
+    return removed
